da1df088782dc089ca541721e4d290de7a35f76f
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-09-21 17:36:41 +0530

    Lecture 2

---
+++ lecture2.cpp
@@ -0,0 +1,44 @@
+//homeworkwork program of basic calculator
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a ,b;
+    cout<<"enter value of a"<<endl;
+    cin>>a;
+
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+
+    int sum=a+b;
+    cout<<"sum of two numbers is="<<sum<<endl;
+
+    int sub= a-b;
+    cout<<"subtraction of two numbers is="<<sub<<endl;
+
+    int multi=a*b;
+    cout<<"multiplication of a and b is="<<multi<<endl;
+
+    int division=a/b;
+    cout<<"division of two numbers is="<<division<<endl;
+
+    int modulo= a%b;
+    cout<<"modulo of a and b is="<<modulo<<endl;
+}
+
+// #include<iostream>
+// using namespace std;
+// int main() 
+// {
+//     int a,b;
+//     cout<<"enter value of a=";
+//     cin>>a;
+
+//     cout<<"enter value of b=";
+//     cin>>b;
+
+//     int sum =a+b;
+//     cout<<"sum="<<sum<<endl;
+//     return 0;
+
+// }
